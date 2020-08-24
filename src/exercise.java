893e25240b6bd2dd7af7e59d4d77afa135c5669f
MilkyasEyob.Habte <milkyas23>
2020-08-24 21:37:47 +0200

    Initial commit

---
+++ src/exercise.java
@@ -0,0 +1,8 @@
+
+public class exercise {
+    public static void main(String[] args) {
+        String output = "wellcome back";
+        System.out.println(output);
+        System.out.println(output.toUpperCase());
+    }
+}
